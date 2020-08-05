044f4e05e8d6621ac7fa7a722ee680b23b6c7252
ray0426 <ray0426you@gmail.com>
2020-08-05 21:14:44 +0800

    make main window with some blocks

--- imgs/src/main_win.py
+++ imgs/src/main_win.py
@@ -10,20 +10,56 @@ class main_Window ():
     def create_window(self):
         self.window = tk.Tk()
         self.window.title('Img Player')
-        self.window.geometry('1024x576')
-        self.menu_bar = tk.Frame(self.window, bg='white')
-        self.menu_bar.pack(side=tk.TOP, fill='x', padx=2, pady=2, )
-        self.content = tk.Frame(self.window, bg='white')
-        self.content.pack(side=tk.BOTTOM, fill='x', padx=2, pady=2)
-        self.left_frame = tk.Frame(self.content, bg='yellow')
-        self.left_frame.pack(side=tk.LEFT, fill='y')
-        self.right_frame = tk.Frame(self.content, bg='green')
-        self.right_frame.pack(side=tk.RIGHT, fill='y')
-        #self.left_button = tk.Button(self.bottom_frame, text='pre', fg='red', command=self.img_pre)
-        #self.left_button.pack(side=tk.LEFT)
+        self.window.geometry('1024x600')
+        self.window.minsize(1024, 600) 
+        self.window.rowconfigure(0, minsize=600, weight=1) # height (row height)
+        self.window.columnconfigure(0, minsize=124, weight=1)  # width (column width)
+        self.window.columnconfigure(1, minsize=900, weight=1)  # width (column width)
+        self.create_menu()
+        #self.content = tk.Frame(self.window, bg='white')
+        #self.content.pack(side=tk.BOTTOM, fill='x', padx=2, pady=2)
+        self.fr_info = tk.Frame(self.window, bg='yellow')
+        self.fr_exhibit = tk.Frame(self.window, bg='blue')
+        self.btn_open = tk.Button(self.fr_info, text="Open")
+        self.btn_save = tk.Button(self.fr_info, text="Save As...")
+        self.btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+        self.btn_save.grid(row=1, column=0, sticky="ew", padx=5)
+        self.fr_info.grid(row=0, column=0, sticky="nesw")
+        self.fr_exhibit.grid(row=0, column=1, sticky="nesw")
+        self.create_exhibit()
 
-        #self.right_button = tk.Button(self.bottom_frame, text='next', fg='blue', command=self.img_next)
-        #self.right_button.pack(side=tk.LEFT)
+    def create_menu(self):
+        self.menu = tk.Menu(self.window)
+        self.window.config(menu=self.menu)
+        self.new_item = tk.Menu(self.menu, tearoff=0)
+        self.new_item.add_command(label='New', command=lambda: print("menu-new"))
+        self.menu.add_cascade(label='File', menu=self.new_item)
+
+    def create_exhibit(self):
+        for i in range(3):
+            self.fr_exhibit.rowconfigure(i, weight=1, minsize=200)
+
+            for j in range(5):
+                self.fr_exhibit.columnconfigure(j, weight=1, minsize=180)
+
+                frame = tk.Frame(
+                    master=self.fr_exhibit,
+                    relief=tk.RAISED,
+                    borderwidth=1
+                )
+                frame.grid(row=i, column=j, padx=5, pady=5)
+
+                img = Image.open('.\\pics\\59060131_p0_master1200.jpg')
+                [imageSizeWidth, imageSizeHeight] = img.size
+                img = img.resize((168, 168), Image.ANTIALIAS)
+                self.photo = ImageTk.PhotoImage(img)
+                self.label = tk.Label(frame, image=self.photo)
+                self.label.pack()
 
 
-    
+                label = tk.Label(master=frame, text=f"Row {i}\nColumn {j}")
+                label.pack(padx=5, pady=5)
+
+
+if __name__ == '__main__':
+    main_window = main_Window()
