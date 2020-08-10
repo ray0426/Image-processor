f9442371d6781292c0d07913e40a72771d82e60d
ray0426 <ray0426you@gmail.com>
2020-08-10 20:44:36 +0800

    save image frames to self

--- imgs/src/main_win.py
+++ imgs/src/main_win.py
@@ -1,10 +1,12 @@
 import tkinter as tk
 from PIL import Image, ImageTk
+import numpy as np
 
 class main_Window ():
     def __init__(self, img_data):
         self.img_index = 0
-        self.img_data = img_data
+        self.img_frames = ()
+        self.img_info = img_data
         self.create_window()
         self.window.mainloop()
     
@@ -25,48 +27,72 @@ class main_Window ():
         self.btn_save.grid(row=1, column=0, sticky="ew", padx=5)
         self.fr_info.grid(row=0, column=0, sticky="nesw")
         self.fr_exhibit.grid(row=0, column=1, sticky="nesw")
-        self.refresh_exhibit()
+        #self.refresh_exhibit()
+        self.create_exhibit()
+        self.refresh_img()
 
     def create_menu(self):
         self.menu = tk.Menu(self.window)
         self.window.config(menu=self.menu)
         self.new_item = tk.Menu(self.menu, tearoff=0)
-        self.new_item.add_command(label='New', command=lambda: print("menu-new"))
+        self.new_item.add_command(label='New', command=self.test)
         self.menu.add_cascade(label='File', menu=self.new_item)
 
-    # refresh the exhibit place of image
-    def refresh_exhibit(self):
-        print(int(len(self.img_data)))
-        for i in range(int(len(self.img_data) / 5) + 1):
-            self.fr_exhibit.rowconfigure(i, weight=1, minsize=220)
-
+    def create_exhibit(self): #tkinter.Frame
+        self.img_frames = np.empty((0), dtype=tk.Frame)
+        self.imgs = np.empty((0), dtype=tk.Label)
+        self.img_names = np.empty((0), dtype=tk.Label)
+        for i in range(3):
+            self.fr_exhibit.rowconfigure(i, minsize=220)
             for j in range(5):
-                self.fr_exhibit.columnconfigure(j, weight=1, minsize=180)
-                if (i * 5 + j) < len(self.img_data):
-                    print("ya")
-                    frame = tk.Frame(
-                        master=self.fr_exhibit,
-                        relief=tk.RAISED,
-                        borderwidth=1
-                    )
-                    frame.grid(row=i, column=j, ipadx=5, ipady=5, padx=5, pady=5, sticky="nw")
+                self.fr_exhibit.columnconfigure(j, minsize=180)
+                frame = tk.Frame(
+                    master=self.fr_exhibit,
+                    relief=tk.RAISED,
+                    borderwidth=1
+                )
+                frame.bind('<Enter>', self.enter)
+                frame.bind('<Leave>', self.leave)
+                frame.grid(row=i, column=j, ipadx=5, ipady=5, padx=5, pady=5, sticky="nw")
+                self.img_frames = np.append(self.img_frames, frame)
 
-                    img = Image.open('.\\pics\\59060131_p0_master1200.jpg')
-                    img = img.resize(self.img_resize(img.size), Image.ANTIALIAS)
-                    photo = ImageTk.PhotoImage(img)
-                    self.label = tk.Label(frame, image=photo, height=150, width=160)
-                    self.label.image = photo # the image should be saved because photo is a local variable
-                    self.label.pack(pady=5)
+                label_img = tk.Label(frame)
+                label_img.pack(pady=5)
+                self.imgs = np.append(self.imgs, label_img)
 
-                    label = tk.Label(master=frame, text=f"Row {i}\nColumn {j}")
-                    label.pack()
+                label_name = tk.Label(master=frame, text=f"Row {i}\nColumn {j}")
+                label_name.pack()
+                self.img_names = np.append(self.img_names, label_name)
 
+    def refresh_img(self):
+        index = 0
+        for img_info_single in self.img_info:
+            img = Image.open(img_info_single['src'])
+            img = img.resize(self.img_resize(img.size), Image.ANTIALIAS)
+            photo = ImageTk.PhotoImage(img)
+            self.imgs[index].configure(image=photo, height=150, width=160)
+            self.imgs[index].image = photo
+            index = index + 1
+            
     # function to resize the image with correct width height rate
     def img_resize(self, size):
         if size[0] > size[1]:
             return [156, int(156 * size[1] / size[0])]
         else:
             return [int(156 * size[0] / size[1]), 156]
+
+    def enter(self, event):
+        print("Enter")
+        print(event)
+
+    def leave(self, event):
+        print("Leave\n")
+
+    def test(self):
+        print(self.img_frames)
+        #print(self.imgs)
+        for img in self.imgs:
+            print(img)
 
 if __name__ == '__main__':
     main_window = main_Window()
