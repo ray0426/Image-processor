00864678502487cc7a3d209f31ede766be62768d
ray0426 <ray0426you@gmail.com>
2020-08-13 16:29:52 +0800

    finish tag functions v1

--- imgs/src/test_file/test_tags.py
+++ imgs/src/test_file/test_tags.py
@@ -1,28 +1,55 @@
+import json
+import time
 
-tag_exist = ['apple', 'banana', 'orange']
+class Img_Tag ():
+    def load_tags(self):
+        print("load tags")
+        with open('data_tags.json', 'r', encoding='utf-8') as f:
+            self.data_tag = json.load(f)
+        print("load tags success!")
+        return self.data_tag
 
-def tags_to_id(tags):
-    ids = []
-    for tag in tags:
-        if new_tag(tag):
-            ids.append(create_tag(tag))
+    def write_tags(self):
+        print("write tags")
+        with open('data_tags.json', 'w', encoding='utf-8') as f:
+            json.dump(self.data_tag, f, indent=4)
+        print("write tags success!")
+
+    def tags_to_id(self, tags):
+        ids = []
+        for tag in tags:
+            if not self.exist_tag(tag):
+                ids.append(self.create_tag(tag)['tag_id'])
+            else:
+                ids.append(self.find_by_name(tag)['tag_id'])
+        return ids
+
+    def exist_tag(self, tag):
+        if any(t['tag_name'] == tag for t in self.data_tag):
+            return True
         else:
-            ids.append(find_by_name(tag))
-    return ids
+            return False
 
-def new_tag(tag):
-    if tag in tag_exist:
-        return False
-    else:
-        return True
+    def create_tag(self, tag):
+        new_tag = {}
+        new_tag['tag_id'] = len(self.data_tag)
+        new_tag['tag_name'] = tag
+        new_tag['create_time'] = time_now()
+        self.data_tag.append(new_tag)
+        print("== new tag name: \"" + new_tag['tag_name'] + "\", tag ID: " + str(new_tag['tag_id']))
+        print("== create time: " + str(new_tag['create_time']) + "\n")
+        return new_tag
 
-def create_tag(tag):
-    tag_exist.append(tag)
-    return len(tag_exist) - 1
+    def find_by_name(self, tag):
+        return [t for t in self.data_tag if t['tag_name'] == tag][0]
 
-def find_by_name(tag):
-    return tag_exist.index(tag)
+def time_now():
+    return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
 if __name__ == '__main__':
-    print(tags_to_id(['banana', 'orange', 'peach']))
-    print(tag_exist)
+    img_tag = Img_Tag()
+    img_tag.load_tags()
+    print(img_tag.tags_to_id(["banana", "apple"]))
+    print(img_tag.tags_to_id(["apple", "pie", "banana"]))
+    print(img_tag.tags_to_id(["pie", "banana", "apple"]))
+    img_tag.write_tags()
